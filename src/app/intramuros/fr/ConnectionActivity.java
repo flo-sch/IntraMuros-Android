64ba84f41ccb4837e0922b0b6a0c68e52c7337e5
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2012-07-03 17:32:38 +0200

    First commit with sources

---
+++ src/app/intramuros/fr/ConnectionActivity.java
@@ -0,0 +1,87 @@
+package app.intramuros.fr;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ConnectionActivity extends Activity implements OnClickListener {
+	private static final String TAG = "IM-CA";
+	private TextView label_username, error_empty_username = null;
+	private TextView label_email, error_empty_email = null;
+	private EditText field_username = null;
+	private EditText field_email = null;
+	private Button submit_connection = null;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connection);
+        
+        this.initProperties();
+    }
+    
+    private void initProperties() {
+    	this.label_username = (TextView)findViewById(R.id.label_username);
+    	this.label_email = (TextView)findViewById(R.id.label_email);
+    	this.field_username = (EditText)findViewById(R.id.field_username);
+    	this.field_email = (EditText)findViewById(R.id.field_email);
+    	this.error_empty_username = (TextView)findViewById(R.id.error_empty_username);
+    	this.error_empty_email = (TextView)findViewById(R.id.error_empty_email);
+    	this.submit_connection = (Button)findViewById(R.id.submit_connection);
+    	
+    	this.submit_connection.setOnClickListener(this);
+    }
+    
+	@Override
+	public void onClick(View sender) {
+		switch (sender.getId()) {
+			case R.id.submit_connection:
+				try {
+					Log.i(TAG, this.field_username.getText().toString());
+					Log.i(TAG, this.field_email.getText().toString());
+					ArrayList<String> errors = new ArrayList<String>();
+					
+					if (this.field_username.getText().length() == 0) {
+						errors.add(getString(R.string.error_empty_username));
+						Log.i(TAG, "Height : " + this.error_empty_username.getHeight());
+						this.error_empty_username.setHeight(25);
+					} else {
+						Log.i(TAG, "Height : " + this.error_empty_username.getHeight());
+						this.error_empty_username.setHeight(0);
+					}
+					if (this.field_email.getText().length() == 0) {
+						errors.add(getString(R.string.error_empty_email));
+						Log.i(TAG, "Height : " + this.error_empty_email.getHeight());
+						this.error_empty_email.setHeight(25);
+					} else {
+						Log.i(TAG, "Height : " + this.error_empty_email.getHeight());
+						this.error_empty_email.setHeight(0);
+					}
+					
+					if (errors.size() == 0) {
+						Intent mainActivity = new Intent(ConnectionActivity.this, MainActivity.class);
+						
+						startActivity(mainActivity);
+					}
+				}
+				catch (ActivityNotFoundException ANFE) {
+					Log.e(TAG, ANFE.getMessage());
+				}
+				catch (NullPointerException NPE) {
+					Log.e(TAG, NPE.toString());
+				}
+				break;
+		}
+	}
+}
---
+++ src/app/intramuros/fr/IMAPIManager.java
@@ -0,0 +1,25 @@
+package app.intramuros.fr;
+
+import android.content.Context;
+import android.os.Handler;
+
+/**
+ * 
+ * @author Flo Schield Bobby
+ * 
+ * This service is the project's background API-referent
+ * This is a helper for every call to the API
+ * It will format the JSON objects to custom classes
+ * And send callback to the sender 
+ *
+ */
+public class IMAPIManager {
+	/**
+     * Constructor. Prepares a new BluetoothChat session.
+     * @param context  The UI Activity Context
+     * @param handler  A Handler to send messages back to the UI Activity
+     */
+    public IMAPIManager(Context context, Handler handler) {
+    	
+    }
+}
