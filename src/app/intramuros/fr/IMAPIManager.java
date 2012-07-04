d6d177f7fdbdc1db06327302715ff552784b6015
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2012-07-04 21:11:40 +0200

    First try of parsing JSON API callback

--- src/app/intramuros/fr/IMAPIManager.java
+++ src/app/intramuros/fr/IMAPIManager.java
@@ -1,7 +1,20 @@
 package app.intramuros.fr;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import android.content.Context;
-import android.os.Handler;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 
 /**
  * 
@@ -14,12 +27,44 @@ import android.os.Handler;
  *
  */
 public class IMAPIManager {
-	/**
-     * Constructor. Prepares a new BluetoothChat session.
-     * @param context  The UI Activity Context
-     * @param handler  A Handler to send messages back to the UI Activity
-     */
-    public IMAPIManager(Context context, Handler handler) {
+	private static final String TAG = "IM-APIManager";
+	public static String baseUrl = "http://intramurosapp.com/";
+	
+    public IMAPIManager(Context context) {
     	
+    }
+    
+    public String callAPIPath(String path, String method, ArrayList<String> parameters) {
+    	StringBuilder builder = new StringBuilder();
+    	HttpClient client = new DefaultHttpClient();
+    	Log.i(TAG, baseUrl + path);
+    	HttpGet completePath = new HttpGet(baseUrl + path);
+    	
+    	try {
+    		HttpResponse callback = client.execute(completePath);
+    		int statusCode = callback.getStatusLine().getStatusCode();
+    		
+    		if (statusCode >= 200 && statusCode < 300) {
+    			HttpEntity entity = callback.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+    		} else if (statusCode >= 400) {
+    			Log.e(TAG, "Error, statusCode : " + statusCode);
+    			// throw a custom exception
+    		}
+    	}
+    	catch (ClientProtocolException CPE) {
+    		Log.e(TAG, CPE.getMessage());
+    	}
+    	catch (IOException IOE) {
+    		Log.e(TAG, IOE.getMessage());
+		}
+    	
+		return builder.toString();
     }
 }
--- src/app/intramuros/fr/MainActivity.java
+++ src/app/intramuros/fr/MainActivity.java
@@ -1,13 +1,34 @@
 package app.intramuros.fr;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+	private static final String TAG = "IM - MainActivity";
+	public IMAPIManager APIManager = null;
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        this.APIManager = new IMAPIManager(this);
+        
+        try {
+        	String callback = this.APIManager.callAPIPath("api/users/lookup", "GET", null);
+        	JSONObject jObject = new JSONObject(callback);
+        	Log.i(TAG, jObject.length() + " keys");
+        	//Log.i(TAG, callback.toString());
+        }
+        catch (NullPointerException NPE) {
+        	Log.e(TAG, NPE.getMessage());
+        }
+        catch (Exception E) {
+        	Log.e(TAG, E.toString());
+        }
     }
 }
